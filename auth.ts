1347d477f4eafea6de4e186c0368faef89625b12
Henrik Persson <henrikp@srcpa.gov>
2024-03-21 08:03:54 -0500

    Add roles

--- auth.ts
+++ auth.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import authConfig from "@/auth.config"
+
+import { getUserById } from "@/data/user"
 import { prisma } from "@/lib/prisma"
 
 export const {
@@ -9,6 +11,31 @@ export const {
 	signIn,
 	signOut
 } = NextAuth({
+	callbacks: {
+		async session({ token, session }) {
+			console.log({
+				sessionToken: token,
+				session,
+			})
+
+			if (token.sub && session.user) {
+				session.user.id = token.sub
+				session.user.role = token.role
+			}
+			return session
+		},
+		async jwt({ token }) {
+			if (!token.sub) return token
+
+			const existingUser = await getUserById(token.sub)
+
+			if (!existingUser) return token
+
+			token.role = existingUser.role
+
+			return token
+		}
+	},
 	adapter: PrismaAdapter(prisma),
 	session: { strategy: "jwt" },
 	...authConfig,
